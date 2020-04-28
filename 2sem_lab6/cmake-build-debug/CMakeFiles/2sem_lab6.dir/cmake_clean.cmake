acfcc16de1f5f4984cd8dcaba7cf6c9cf43bb3af
Victoris <52378018+sviridchik@users.noreply.github.com>
2020-04-28 19:50:49 +0300

    Add files via upload

---
+++ 2sem_lab6/cmake-build-debug/CMakeFiles/2sem_lab6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2sem_lab6.dir/main.c.o"
+  "2sem_lab6.pdb"
+  "2sem_lab6"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2sem_lab6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
